2d840c9e518b49bc395a2398f1aa474ae9dad06d
gblsl1 <gabrielsousadeveloper@gmail.com>
2023-01-09 13:31:57 -0300

    atualizacoes

--- src/models/tarefasModel.js
+++ src/models/tarefasModel.js
@@ -1,5 +1,8 @@
-const getAll = () => {
-    
+const conexao = require('./conexao')
+
+const getAll = async () => {
+    const tarefas = await conexao.execute('SELECT * FROM tarefas')
+    return tarefas
 }
 
 module.exports = {
--- src/router.js
+++ src/router.js
@@ -2,4 +2,6 @@ const express = require('express')
 
 const router = express.Router()
 
+router.get('/v1/tarefas', (req, res) => res.status(200).send('O projeto está funcionando'))
+
 module.exports = router
